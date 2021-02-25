5138f7c6502edd80804af3d2669b4e7a096ea859
Lourence Linao <lourencelinao13@gmail.com>
2021-02-26 00:17:09 +0800

    yeet

--- src/database.js
+++ src/database.js
@@ -1,10 +1,21 @@
 const mysql = require('mysql');
 
+let host = process.env.DB_HOST
+let user = process.env.DB_USER
+let database = process.env.DB_DATABASE
+let password = process.env.DB_PASS
+
+console.log(host, user, database, password)
+
+if(process.env.NODE_ENV === 'production'){
+console.log('production yeet')    
+}
+
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS
+    host: host,
+    user: user,
+    database: database,
+    password: password
 })
 
 connection.connect((err) => {
