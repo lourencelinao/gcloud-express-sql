52663c58e5c1ada183725b8ee490ae8fa3dd7a1e
Lourence Linao <lourencelinao13@gmail.com>
2021-02-26 02:16:55 +0800

    port

--- src/app.js
+++ src/app.js
@@ -16,4 +16,4 @@ app.get('/books', (req, res) => {
 })
 
 const PORT = process.env.PORT
-app.listen(PORT, console.log('Server is running on port 3000'))
+app.listen(PORT, console.log(`Server is running on port ${PORT}`))
