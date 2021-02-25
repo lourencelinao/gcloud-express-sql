3ea5179d984bd70cc6489dab672212359cf55244
Lourence Linao <lourencelinao13@gmail.com>
2021-02-26 01:17:40 +0800

    database removed

--- src/app.js
+++ src/app.js
@@ -2,17 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const connection = require('./database')
+// const connection = require('./database')
 
 app.get('/', (req, res) => {
     res.send('Peepeepoopoo')
 })
 
-app.get('/books', (req, res) => {
-    connection.query('SELECT * FROM books', (err, result) => {
-        if(err) throw err
-        res.json(result)
-    })
-})
+// app.get('/books', (req, res) => {
+//     connection.query('SELECT * FROM books', (err, result) => {
+//         if(err) throw err
+//         res.json(result)
+//     })
+// })
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, console.log('Server is running on port 3000'))
