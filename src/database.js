023d18c1987513c3e10e03c97c19aaab356d10c4
Lourence Linao <lourencelinao13@gmail.com>
2021-02-26 01:27:05 +0800

    database added

--- src/database.js
+++ src/database.js
@@ -1,26 +1,29 @@
-// const mysql = require('mysql');
+const mysql = require("mysql");
 
-// let host = process.env.DB_HOST
-// let user = process.env.DB_USER
-// let database = process.env.DB_DATABASE
-// let password = process.env.DB_PASS
+let host = process.env.DB_HOST;
+let user = process.env.DB_USER;
+let database = process.env.DB_DATABASE;
+let password = process.env.DB_PASS;
 
-// console.log(host, user, database, password)
+console.log(host, user, database, password);
 
-// if(process.env.NODE_ENV === 'production'){
-// console.log('production yeet')    
-// }
+if (process.env.NODE_ENV === "production") {
+  console.log("production yeet");
+}
 
-// const connection = mysql.createConnection({
-//     host: host,
-//     user: user,
-//     database: database,
-//     password: password
-// })
+const connection = mysql.createConnection({
+  host: host,
+  user: user,
+  database: database,
+  password: password,
+});
 
-// connection.connect((err) => {
-//     if(err) throw err
-//     console.log('Database mounted')
-// })
+connection.connect((err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Database mounted");
+  }
+});
 
-// module.exports = connection
+module.exports = connection;
